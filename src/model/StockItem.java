8d8b8d70b3e1dd113f7c2a91f1f58b3d1f96df4b
Luís Gustavo <luisgustavo.teodoro52@gmail.com>
2024-11-21 13:56:02 -0300

    Criação lógica de estoque

---
+++ src/model/StockItem.java
@@ -0,0 +1,26 @@
+package model;
+
+public class StockItem {
+	private Product product;
+	private int quantity;
+	
+	public StockItem(String product, int quantity){
+		String productData[] = DataBase.selectProduct(product);
+		
+		this.product = new Product();
+		this.product.setDescription(productData[0]);
+		this.product.setPrice(Double.parseDouble(productData[1]));
+	}
+	
+	public void increaseStock(int valueToIncrease) {
+		this.quantity += valueToIncrease;
+	}
+	
+	public void decreaseStock(int valueToDecrease) {
+		this.quantity += valueToDecrease;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+}
